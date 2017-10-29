e058474361073fe2a551d29abbbb6797a27e9356
Sakai Toma <o_edo@hotmail.com>
2017-10-29 22:30:40 +0100

    Completed code skeleton.

--- static/js/circular-slider.js
+++ static/js/circular-slider.js
@@ -7,45 +7,96 @@ $(function(){
     items : null,
     angle : 0,
     vectors: [],
+    radius : 0,
 
-    init : function(container){
+    init : function(container, radius,){
       this.container = container;
-      const items = container.find("> div > img");
-      this.angle = items && items.length > 0 ? 360/items.length : 0;
+      this.radius   = radius;
+      const items = container.find(".slide");
+      this.setActive( this.container.find(".slide").first() );
+      this.angle = items.length > 0 ? 360/items.length : 0;
+      this.eventSubsribe();
       this.computetPositions();
-      this.moveThumbnail();
+      this.moveImages();
+      const self = this;
+      setTimeout( function()  { self.round(); }, 2000 ) ;
+    },
+
+    eventSubsribe(){
+      const self = this;
+      self.container.find(".slide").hover( function(){
+        self.setActive( $(this) );
+      });
     },
 
     degreeToRadian(degree){
       return (Math.PI * degree ) / 180;
     },
 
+    random(min, max){
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+
     computetPositions(){
       const self = this;
-       self.container.find("> div > img").each( (index, element) => {
+       self.container.find(".slide").each( (index, element) => {
          const angleDegree = index* self.angle;
          const angleRadian = self.degreeToRadian(angleDegree);
-         const coordinateX = 500 * Math.cos(angleRadian );
-         const coordinateY = 500 * Math.sin( angleRadian );
-         self.vectors.push([angleDegree, angleRadian, coordinateX, coordinateY]);
+         const coordinateX = self.radius * Math.cos( angleRadian );
+         const coordinateY = self.radius * Math.sin( angleRadian );
+         self.vectors.push([coordinateX, coordinateY]);
        });
     },
 
-    moveThumbnail(){
+    removeActive(){
+      this.container.find(".active").remove();
+    },
+
+    setActive( target ){
+      this.removeActive();
+      $(target).siblings(".slide").removeClass("red-border");       
+      var clickedClone = $(target).clone().removeClass().css('transform', '').addClass("active");
+      this.container.append(clickedClone);
+    },
+
+    moveImages(){
       const self = this;
-       self.container.find("> div > img").each( (index, element) => {
+       self.container.find(".slide").each( (index, element) => {
          const vector = self.vectors[index];
-         $(element).css(
-           {
-            'transform-origin': '50% 50%',
-             transform: `translateX(${vector[2]}px) translateY(${vector[3]}px) rotate(${vector[0]}deg)`,
-             width: '50px',
-             height: '50px',
-           });
+         const hasRotation = self.random(0,10) > 8 ? true : false;
+         if(hasRotation){
+           $(element)
+            .css({
+               transform: `translateX(${vector[0]}px) translateY(${vector[1]}px) rotate(${self.random(1,20)*360}deg) scale(0.3)`,
+             });
+          }
+          else{
+            $(element)
+             .css({
+                transform: `translateX(${vector[0]}px) translateY(${vector[1]}px) scale(0.3)`,
+              })
+          }
        });
+
+    },
+
+    round(){
+      const self = this;
+      self.container.find(".slide").each( (index, element) => {
+        setTimeout(
+    			( function(arg1) {
+    				return function() {
+              $(arg1).addClass("red-border").siblings(".slide").removeClass("red-border");
+              $(arg1).trigger('mouseover');
+    				};
+    			} ) ( element ),
+    			index*200
+    		);
+     });
     }
   };
 
-  circularSlider.init( $("#circular-slider") );
 
+
+  circularSlider.init( $("#circular-slider"), $(window).width() / 2 -60);
 });
